143628bbe96b007793dd98620526ea2d0bbc2d50
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2023-04-18 17:56:15 -0300

    Update newArticleElements.js

--- cypress/support/pages/newArticle/newArticleElements.js
+++ cypress/support/pages/newArticle/newArticleElements.js
@@ -1,6 +1,6 @@
 export const newArticleElements = {
     buttonNewPost: '.container > .nav > :nth-child(2) > .nav-link',
-    insertArticleTitle: 'fieldset.form-group input.form-control.form-control-lg[placeholder="Article Title"]',
+    insertArticleTitle: 'fieldset.form-group input.form-control.form-control-lg',
     insertSubTitle: ':nth-child(2) > .form-control',
     insertArticle: ':nth-child(3) > textarea',
     publishArticleButton: '.btn',
