558214b0bb102e0184c631d253c3033f6e3fefbb
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2023-04-19 09:16:53 -0300

    Update myArticles.cy.js

--- cypress/e2e/cenarios/myArticles.cy.js
+++ cypress/e2e/cenarios/myArticles.cy.js
@@ -3,7 +3,7 @@ const faker = require('faker-br')
 import MyArticles from "../../support/pages/myArticles/myArticles"
 
 describe('CENÁRIO DE SUCESSO - MEUS ARTIGOS', () => {
-    it('Espera que consiga deletar artigo', () => {
+    it('Espera que consiga publicar um comentário em um artigo', () => {
         const comments = faker.lorem.paragraph()
 
         cy.login('email', 'password')
