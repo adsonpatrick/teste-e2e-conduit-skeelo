5239f9cfa65f68b990fce9f244610f731c2bcc81
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2023-04-19 10:44:28 -0300

    Update newArticleElements.js

--- cypress/support/pages/newArticle/newArticleElements.js
+++ cypress/support/pages/newArticle/newArticleElements.js
@@ -1,7 +1,5 @@
 export const newArticleElements = {
     buttonNewPost: '.container > .nav > :nth-child(2) > .nav-link',
-    insertArticleTitle: '#main > div > div > div > div > div > form > fieldset > fieldset:nth-child(1) > input',
-    insertSubTitle: '#main > div > div > div > div > div > form > fieldset > fieldset:nth-child(2) > input',
     insertArticle: ':nth-child(3) > textarea',
     publishArticleButton: '.btn',
     spanErrorArticle: '.error-messages > li'
