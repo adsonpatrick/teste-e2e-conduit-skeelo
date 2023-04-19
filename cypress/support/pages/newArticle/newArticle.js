38ca7db5b370978fec9de960724742069e56b173
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2023-04-19 09:11:44 -0300

    Update newArticle.js

--- cypress/support/pages/newArticle/newArticle.js
+++ cypress/support/pages/newArticle/newArticle.js
@@ -1,33 +1,21 @@
-import { findAllByAltText, findAllByPlaceholderText } from "@testing-library/dom"
 import { newArticleElements } from "./newArticleElements"
+
 class NewArticle {
     static clickNewPost() {
         cy.get(newArticleElements.buttonNewPost).click()
     }
 
     static insertTitle(title) {
-        //cy.findAllByPlaceholderText("Article Title").type(title, {delay: 0}).should("have.value", title)
-        //cy.get('fieldset.form-group').contains('input', 'Article Title').type(title, {delay: 0}).should("have.value", title)
-        //cy.get(newArticleElements.insertArticleTitle).type(title, {delay: 0}).should("have.value", title)
-        //cy.get('input.form-control.form-control-lg[placeholder="Article Title"]').type(title, {delay: 0}).should("have.value", title)
-        cy.get('#main > div > div > div > div > div > form > fieldset > fieldset:nth-child(1) > input').type(title, {delay: 0})
-        //cy.get('input[placeholder^="Article"]').type(title, {delay: 0})
-
+        cy.get(newArticleElements.insertArticleTitle).type(title, {delay: 0})
 
     }
 
     static insertTitleSubject(titleSubject) {
-        //cy.findAllByPlaceholderText(`What's this article about?`).type(titleSubject, {delay: 0}).should("have.value", titleSubject)
-        //cy.contains('What s this article about?').find('input').type(titleSubject, {delay: 0}).should("have.value", titleSubject)
-        //cy.get(newArticleElements.insertSubTitle).type(titleSubject, {delay: 0}).should("have.value", titleSubject)
-        cy.get('#main > div > div > div > div > div > form > fieldset > fieldset:nth-child(2) > input').type(titleSubject, {delay: 0}).should("have.value", titleSubject)
-        //cy.get('input[placeholder$="about?"]').type(titleSubject, {delay: 0})
+        cy.get(newArticleElements.insertSubTitle).type(titleSubject, {delay: 0})
     }
 
     static writeArticle(article) {
-        //cy.findAllByPlaceholderText("Write your article (in markdown)").type(article, {delay: 0}).should("have.value", article)
-        //cy.get('fieldset.form-group').contains('input', 'Write your article (in markdown)').type(article, {delay: 0}).should("have.value", article)
-        cy.get(newArticleElements.insertArticle).type(article, {delay: 0}).should("have.value", article)
+        cy.get(newArticleElements.insertArticle).type(article, {delay: 0})
     }
 
     static submitArticle() {
