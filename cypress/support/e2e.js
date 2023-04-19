afce2a40c982f898c8bf8c33a66dd825c5034cda
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2023-04-18 22:08:32 -0300

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+import '@testing-library/cypress/add-commands'
 import 'cypress-plugin-steps'
 
 // Alternatively you can use CommonJS syntax:
